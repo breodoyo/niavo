64c3df832c437fbdc256f3b006cf92d3fdee2013
breodoyo <adhiambobrender2@gmail.com>
2026-07-25 19:09:44 +0300

    feat(router): Add API route and versioning

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -11,8 +11,30 @@ func New() *chi.Mux {
 	router := chi.NewRouter()
 
 	router.Use(middleware.Logger)
-
+//Public route	
 	router.Get("/", app.HomeHandler)
+
+//API routes	
+	router.Route("/api", func(r chi.Router) {
+
+		r.Route("/v1", func(r chi.Router) {
+
+			r.Get("/", app.HomeHandler)
+
+			r.Route("/users", func(r chi.Router) {
+
+			})
+			r.Route("/organizations", func(r chi.Router) {
+
+			})
+			r.Route("/workflows", func(r chi.Router) {
+
+			})
+			r.Route("/workitems", func(r chi.Router) {
+
+			})
+		})
+	})
 
 	return router
 }
