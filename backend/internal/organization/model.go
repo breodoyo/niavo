1d71f6cf4715b6b92aa3b1f2a8bc2e123ffac49f
breodoyo <adhiambobrender2@gmail.com>
2026-07-27 11:01:45 +0300

    Add organization model

--- backend/internal/organization/model.go
+++ backend/internal/organization/model.go
@@ -1,1 +1,10 @@
 package organization
+
+import "time"
+
+type Organization struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Slug      string    `json:"slug"`
+	CreatedAt time.Time `json:"created_at"`
+}
