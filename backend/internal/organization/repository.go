a83f29d2cb46b43274c5223e85ad82591c3aa3bf
breodoyo <adhiambobrender2@gmail.com>
2026-07-27 22:41:53 +0300

    Add organization Repository

--- backend/internal/organization/repository.go
+++ backend/internal/organization/repository.go
@@ -1,1 +1,43 @@
 package organization
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Repository struct {
+	db *pgxpool.Pool
+}
+
+func NewRepository(db *pgxpool.Pool) *Repository {
+	return &Repository{
+		db: db,
+	}
+}
+func (r *Repository) CreateOrganization(
+	ctx context.Context,
+	org Organization,
+) (Organization, error) {
+
+	query := `
+	INSERT INTO organizations(name, slug)
+	VALUES($1, $2)
+	RETURNING id, name, slug, created_at
+	`
+	err := r.db.QueryRow(
+		ctx,
+		query,
+		org.Name,
+		org.Slug,
+	).Scan(
+		&org.ID,
+		&org.Name,
+		&org.Slug,
+		&org.CreatedAt,
+	)
+	if err != nil {
+		return Organization{}, err
+	}
+	return org, nil
+}
