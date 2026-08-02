3d6ef5dc629229e758f5a9586ee37c9e6d8d2caf
breodoyo <adhiambobrender2@gmail.com>
2026-08-02 23:27:13 +0300

    Add MockUserService for handler tests

--- backend/internal/user/mock_service_test.go
+++ backend/internal/user/mock_service_test.go
@@ -1,1 +1,20 @@
 package user
+
+import "context"
+
+type MockUserService struct {}
+
+func (m *MockUserService) CreateUser(ctx context.Context, req CreateUserRequest) (User, error) {
+	return User{}, nil
+}
+func (m *MockUserService) ListUsers(ctx context.Context) ([]User, error) {
+	return []User{}, nil
+}
+func (m *MockUserService) GetUser(ctx context.Context, id string) (User, error) {
+	return User{
+		ID: id,
+		Email: "ksks@gmail.com",
+		FirstName: "Nan",
+		LastName: "Luis",
+	}, nil
+}
